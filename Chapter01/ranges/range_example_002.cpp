3df6ca5ad7ffae9db08cb9b73b850920f9839b7e
Florin Chertes <FlorinChertes@computer.org>
2019-06-22 09:10:20 +0200

    chapter01 ranges

--- Chapter01/ranges/range_example_002.cpp
+++ Chapter01/ranges/range_example_002.cpp
@@ -8,18 +8,15 @@
 
 int main()
 {
-    for (int i : ranges::view::ints(1, ranges::unreachable)
+    auto range_int = ranges::view::ints(1, ranges::unreachable)
                 | ranges::view::transform([](int i) {return i*i;})
-                | ranges::view::take(10))
+                | ranges::view::take(10);
+
+    for (int i : range_int)
         std::cout << i << ", ";
     std::cout << std::endl;
 
-    const int sum = ranges::accumulate(
-        ranges::view::ints(1, ranges::unreachable)
-                | ranges::view::transform([](int i) {return i*i;})
-                | ranges::view::take(10)
-        ,0);
-
+    const int sum = ranges::accumulate(range_int, 0);
     std::cout << "sum = " << sum;
     std::cout << std::endl;
  

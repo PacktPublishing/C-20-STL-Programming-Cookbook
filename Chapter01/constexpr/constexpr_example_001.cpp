65f47fb605430a2f5e66992604731452791293c9
Florin Chertes <FlorinChertes@computer.org>
2019-09-11 21:12:09 +0200

    constexpr example changes

--- Chapter01/constexpr/constexpr_example_001.cpp
+++ Chapter01/constexpr/constexpr_example_001.cpp
@@ -7,40 +7,40 @@
 //-----------------------------------------------------------------------------
 constexpr double harmonic_mean(int a, int b)
 {
-	double ret_val = 0.0;
-	ret_val = 2.0 * a * b / (a + b);
+    double ret_val = 0.0;
+    ret_val = 2.0 * a * b / (a + b);
 
-	return ret_val;
+    return ret_val;
 }
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void test_type_func_012()
 {
-	std::cout << "*** test 12  harmonic mean of two numbers at compile time ***" << std::endl;
+    std::cout << "*** test 12  harmonic mean of two numbers at compile time ***" << std::endl;
 
-	constexpr int a = 3;
-	constexpr int b = 5;
+    constexpr int a = 3;
+    constexpr int b = 5;
 
-	constexpr double hm = harmonic_mean(a, b);
-	static_assert(hm == 3.75);
+    constexpr double hm = harmonic_mean(a, b);
+    static_assert(hm == 3.75);
 
-	std::cout << "Value computed at compile time: " << hm << std::endl;
+    std::cout << "Value computed at compile time: " << hm << std::endl;
 }
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void test_type_func_013()
 {
-	std::cout << "*** test 13  harmonic mean of two numbers at run time ***" << std::endl;
+    std::cout << "*** test 13  harmonic mean of two numbers at run time ***" << std::endl;
 
-	int a = 3;
-	const int b = 5;
+    int a = 3;
+    const int b = 5;
 
-	const double hm = harmonic_mean(a, b);
-	assert(hm == 3.75);
+    const double hm = harmonic_mean(a, b);
+    assert(hm == 3.75);
 
-	std::cout << "Value computed at run time: " << hm << std::endl;
+    std::cout << "Value computed at run time: " << hm << std::endl;
 }
 
 int main (int argc, char *argv[])
@@ -50,6 +50,6 @@ int main (int argc, char *argv[])
     test_type_func_012();
     test_type_func_013();
 
-
     return 0;
 }
+

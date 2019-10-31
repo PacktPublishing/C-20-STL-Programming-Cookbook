c44a696795a8501d75282a8df6a25de33059eca5
Florin Chertes <FlorinChertes@computer.org>
2019-10-31 19:52:33 +0100

    using refinement for low level memory optimisation

--- Chapter10/concepts_example_002_21.cpp
+++ Chapter10/concepts_example_002_21.cpp
@@ -1,14 +1,9 @@
-#include <type_traits>
-
-#include <string>
-
 #include <set>
 #include <list>
 #include <vector>
 
-#include <cassert>
-
 #include <iostream>
+#include <iterator>
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -42,6 +37,22 @@ template <typename T, typename U>
 concept bool Same_as()
 {
     return std::is_same<T, U>::value;   
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+template<typename I>
+using iterator_category_t =
+    typename std::iterator_traits<I>::iterator_category;
+
+
+template <typename I, typename It_Category>
+concept bool Not_same_category_as()
+{
+	return std::is_same<
+        iterator_category_t<I>,
+        It_Category
+                >::value == false;
 }
 
 //-----------------------------------------------------------------------------
@@ -121,6 +132,32 @@ concept bool Input_iterator()
         // It can be incremented
         { ++i } -> I&;        
     };
+}
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+template<Input_iterator I>
+void advance (I& iter, int n)
+{
+    while (n != 0)
+    {
+        ++iter;
+        n--;
+    }
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+template<Input_iterator I>
+int distance (I first, I limit)
+{
+    int n = 0;
+    for(; first != limit; ++first, ++n)
+    {
+        ;
+    }
+    return n;
 }
 
 //-----------------------------------------------------------------------------
@@ -204,20 +241,17 @@ bool in (A const& assoc, K const& key)
     return assoc.find(key) != assoc.end();
 }
 
+
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename I>
 concept bool Forward_iterator()
 {
-    return requires (I i)
+    return Input_iterator<I>() && requires (I i)
     {
-        typename value_type_t<I>;
-
-        // It can be dereferenced
-        { *i } -> const value_type_t<I>&;
-
-        // It can be incremented
-        { ++i } -> I&;        
+        typename iterator_category_t<I>;
+        requires Not_same_category_as<
+                I, std::input_iterator_tag>();
     };
 }
 
@@ -319,23 +353,68 @@ bool binary_search(I first, I limit, T const& value)
         return true;
 }
 
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+template <Input_iterator I, typename T>
+	requires Same_as<T, value_type_t<I>>()
+void save_to_vector(I first, I limit, std::vector<T>& v)
+{
+	for(;first != limit; ++first)
+	{
+		v.push_back(*first);
+	}
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+template <Forward_iterator I, typename T>
+	requires Same_as<T, value_type_t<I>>()
+void save_to_vector(I first, I limit, std::vector<T>& v)
+{
+	v.reserve(distance(first, limit));
+	v.insert(v.begin(), first, limit);
+}
+
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 int main()
 {
-//-----------------------------------------------------------------------------
-    int array[] = {1,2,4,5,6,7};
-    const size_t len = sizeof(array) / sizeof(int);
     const int value = 5;
-    bool bs_found = binary_search(array, array + len, value);
+	bool bs_found;
+
+//-----------------------------------------------------------------------------
+	std::istream_iterator<int> cin_it(std::cin);
+	std::istream_iterator<int> eos;
+
+	std::vector<int> v_sink_input;
+	save_to_vector(cin_it, eos, v_sink_input);
+    bs_found = binary_search(v_sink_input.begin(), v_sink_input.end(), value);
+	std::cout << std::endl;
     std::cout << std::boolalpha <<
-        "binary search array found = " << bs_found << std::endl;
+        "binary search in vector sink input it found = " << bs_found << std::endl;
+
+//-----------------------------------------------------------------------------
+    std::list<int> l_source = {1,2,4,5,6,7};
+
+	std::vector<int> v_sink_forward;
+	save_to_vector(l_source.begin(), l_source.end(), v_sink_forward);
+    bs_found = binary_search(v_sink_forward.begin(), v_sink_forward.end(), value);
+    std::cout << std::boolalpha <<
+        "binary search in vector sink forword it found = " << bs_found << std::endl;
 
 //-----------------------------------------------------------------------------    
     std::list<int> l = {1,2,4,5,6,7};
     bs_found = binary_search(l.begin(), l.end(), value);
     std::cout << std::boolalpha <<
         "binary search list found = " << bs_found << std::endl;
+
+//-----------------------------------------------------------------------------
+    int array[] = {1,2,4,5,6,7};
+    const size_t len = sizeof(array) / sizeof(int);
+    bs_found = binary_search(array, array + len, value);
+    std::cout << std::boolalpha <<
+        "binary search array found = " << bs_found << std::endl;
 
 //-----------------------------------------------------------------------------    
     std::vector<int> vect = {1,2,4,5,6,7};

4f1dd3d670881b3fb0408402beca466a64993230
FlorinChertes <FlorinChertes@computer.org>
2019-12-06 20:29:24 +0100

    adapt to standard draft

--- Chapter10/concepts_example_002_1.cpp
+++ Chapter10/concepts_example_002_1.cpp
@@ -3,50 +3,36 @@
 
 #include <iostream>
 
-//-----------------------------------------------------------------------------
+///-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename T>
-concept bool Equality_comparable()
+concept Equality_comparable = requires (T a, T b)
 {
-    return requires (T a, T b)
-    {
-        {a == b} -> bool;
-        {a != b} -> bool;
-    };   
-}
+    {a == b} -> bool;
+    {a != b} -> bool;
+};
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename R>
-concept bool Range()
+concept Range = requires (R range)
 {
-    return requires (R range)
-    {
-        begin(range);
-        end(range);
-    };   
-}
+    range.begin();
+    range.end();
+};
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename T, typename U>
-concept bool Same()
-{
-    return std::is_same<T, U>::value;   
-}
+concept Same = std::is_same<T, U>::value;
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template<typename T>
-requires requires { typename T::value_type; }
 struct value_type;
-
-template<typename T>
-using Value_type = typename value_type<T>::type;
 
 // The value_type of a class is a member type
 template<typename T>
-requires requires { typename T::value_type; }
 struct value_type
 {
     using type = typename T::value_type;
@@ -66,38 +52,33 @@ struct value_type<T[N]>
     using type = T;
 };
 
+template<typename T>
+using value_type_t = typename value_type<T>::type;
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
-template<Range R, Equality_comparable T>
-  requires Same<T, Value_type<R>>()
+template<typename R, typename T>
+  requires Range<R> &&
+           Equality_comparable<T> &&
+           Same<value_type_t<R>, T>
 bool in (R const& range, T const& value)
 {
-    for(Equality_comparable const& x : range)
-    {
-        if(x == value)
-        {
+    for(const auto& x : range) {
+        if(x == value) {
             return true;
         }
     }
     return false;
 }
 
-
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 int main()
 {
-    const std::string value_one("one");
-    const std::string value_two("two");
-    const std::string value_three("three");
-
-    std::vector<std::string> v {value_one, value_two, value_three};
+    std::vector<std::string> v {"one", "two", "three"};
     
-    const bool found = in(v, value_one);
-
+    const bool found = in(v, std::string("two"));
     std::cout << "value was found: " << found << std::endl;
-
 
     return 0;
 }

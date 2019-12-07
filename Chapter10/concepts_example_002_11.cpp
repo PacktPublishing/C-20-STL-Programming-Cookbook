ece7ed5415ed30901de28eb2e38fb626c17c98ed
FlorinChertes <FlorinChertes@computer.org>
2019-12-07 21:27:42 +0100

    adapt to standard draft

--- Chapter10/concepts_example_002_11.cpp
+++ Chapter10/concepts_example_002_11.cpp
@@ -7,42 +7,52 @@
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename T>
-concept bool Equality_comparable()
+concept Equality_comparable = requires (T a, T b)
 {
-    return requires (T a, T b)
-    {
-        {a == b} -> bool;
-        {a != b} -> bool;
-    };   
-}
+    {a == b} -> bool;
+    {a != b} -> bool;
+};
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename T, typename U>
-concept bool Equality_comparable()
-{
-    return requires (T t, U u)
-    {
-        {t == u} -> bool;
-        {u == t} -> bool;        
-        {t != u} -> bool;
-        {u != t} -> bool;        
-    };   
-}
+concept Same = std::is_same<T, U>::value;
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename T, typename U>
-concept bool Same_as()
+concept Equality_comparable_types = requires (T t, U u)
 {
-    return std::is_same<T, U>::value;   
-}
+	{t == u} -> bool;
+	{u == t} -> bool;
+	{t != u} -> bool;
+	{u != t} -> bool;
+};
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+template<typename T>
+struct iterator_type;
+
+// The iterator_type of a class is a member type
+template<typename T>
+struct iterator_type
+{
+    using type = typename T::iterator;
+};
+
+template<typename T>
+using iterator_type_t = typename iterator_type<T>::type;
+
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 // The value_type of a class is a member type
 template<typename T>
-requires requires { typename T::value_type; }
+struct value_type;
+
+// The value_type of a class is a member type
+template<typename T>
 struct value_type
 {
     using type = typename T::value_type;
@@ -67,22 +77,11 @@ using value_type_t = typename value_type<T>::type;
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
-// The iterator of a class is a member type
 template<typename T>
-requires requires { typename T::iterator; }
-struct iterator_type
-{
-    using type = typename T::iterator;
-};
+struct key_type;
 
-template<typename T>
-using iterator_t = typename iterator_type<T>::type;
-
-//-----------------------------------------------------------------------------
-//-----------------------------------------------------------------------------
 // The key_type of a class is a member type
 template<typename T>
-requires requires { typename T::key_type; }
 struct key_type
 {
     using type = typename T::key_type;
@@ -95,87 +94,77 @@ using key_type_t = typename key_type<T>::type;
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename I>
-concept bool Input_iterator()
+concept Input_iterator = requires(I i)
 {
-    return requires(I i)
-    {
-  	    // Must have a value type
-        typename value_type_t<I>;
+	// Must have a value type
+	typename value_type_t<I>;
 
-        // It can be dereferenced
-        { *i } -> const value_type_t<I>&;
+	// It can be dereferenced
+	{ *i } -> const value_type_t<I>&;
 
-        // It can be incremented
-        { ++i } -> I&;        
-    };
-}
+	// It can be incremented
+	{ ++i } -> I&;
+};
+
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename R>
-concept bool Range()
+concept Range = requires (R range)
 {
-    return requires (R range)
-    {
-	    // Must have a value type
-        typename value_type_t<R>;
-	    // Must have an iterator type
-	    typename iterator_t<R>;
+	// Must have a value type
+	typename value_type_t<R>;
+	// Must have an iterator type
+	typename iterator_type_t<R>;
 
-        { begin(range) } -> iterator_t<R>;
-        { end(range) } -> iterator_t<R>;
-        
-        // The iterator type must be an input iterator
-        requires Input_iterator<iterator_t<R>>();
+	{ range.begin() } -> iterator_type_t<R>;
+	{ range.end() } -> iterator_type_t<R>;
 
-        // The value of R is the same as it's
-        // iterator value type
-        requires Same_as< value_type_t<R>,
-                       value_type_t<iterator_t<R>>>();
-    };   
-}
+	// The iterator type must be an input iterator
+	requires Input_iterator<iterator_type_t<R>>;
+
+	// The value of R is the same as it's
+	// iterator value type
+	requires Same< value_type_t<R>,
+				   value_type_t<iterator_type_t<R>> >;
+};
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename S>
-concept bool Sequence()
+concept Sequence = Range<S>() && requires (S seq)
 {
-    return Range<S>() && requires (S seq)
-    {
-        { seq.front() } -> const value_type_t<S>&;
-        { seq.back() } -> const value_type_t<S>&;
-    };
-}
+	{ seq.front() } -> const value_type_t<S>&;
+	{ seq.back() } -> const value_type_t<S>&;
+};
+
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template <typename A>
-concept bool Associative_container()
+concept Associative_container = Range<A> &&
+requires
 {
-    return Range<A>() && 
-    requires {
-        typename key_type_t<A>;
-    } &&   
-    requires (A assoc_c, key_type_t<A> k)
-    {
-        { assoc_c.empty() } -> bool;
-        { assoc_c.size() } -> int;
-        { assoc_c.find(k) } -> iterator_t<A>;
-        { assoc_c.count(k) } -> int;
-        { assoc_c.end() } -> iterator_t<A>;
-    };
-}
+	typename key_type_t<A>;
+} &&
+requires (A assoc_c, key_type_t<A> k)
+{
+	{ assoc_c.empty() } -> bool;
+	{ assoc_c.size() } -> int;
+	{ assoc_c.find(k) } -> iterator_type_t<A>;
+	{ assoc_c.count(k) } -> int;
+	{ assoc_c.end() } -> iterator_type_t<A>;
+};
+
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template<Sequence S, Equality_comparable T>
-    requires Same_as<T, value_type_t<S>>()
+    requires Same<T, value_type_t<S>>
 bool in (S const& seq, T const& value)
 {
-    for(auto const& x : seq)
-    {
-        if(x == value)
-        {
+    for(auto const& x : seq) {
+        if(x == value) {
             return true;
         }
     }
@@ -185,7 +174,7 @@ bool in (S const& seq, T const& value)
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 template<Associative_container A, typename K>
-    requires Same_as<K, key_type_t<A>>()
+    requires Same<K, key_type_t<A>>
 bool in (A const& assoc, K const& key)
 {
     return assoc.find(key) != assoc.end();
@@ -195,16 +184,12 @@ bool in (A const& assoc, K const& key)
 //-----------------------------------------------------------------------------
 int main()
 {
-    const std::string value_one("one");
-    const std::string value_two("two");
-    const std::string value_three("three");
-    
-    std::vector<std::string> v {value_one, value_two, value_three};
-    const bool found_seq = in(v, value_one);
-    std::cout << "value was found in sequence: " << found_seq << std::endl;
+    std::vector<std::string> v {"one", "two", "three"};
+    const bool found = in(v, std::string("two"));
+    std::cout << "value was found: " << found << std::endl;
 
-    std::set<std::string> s {value_one, value_two, value_three};
-    const bool found_assoc = in(s, value_one);
+    std::set<std::string> s {"one", "two", "three"};
+    const bool found_assoc = in(s, std::string("two"));
     std::cout << "value was found in assoc container: " << found_assoc << std::endl;
 
     return 0;
